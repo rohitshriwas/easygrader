0dbf85eb2c5cc939dc1ed9e0078179aeec702aab
Saurabh Johri <saurabhxj@gmail.com>
2025-05-27 12:33:40 +0530

    Enable cutoffs on page load

---
+++ scripts/cutoffs.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var cutOffs = JSON.parse(document.querySelector("[name='cut_offs']").value);
+  document.getElementById("plotHistButton").click();
+
+  cutOffs.forEach((element) => {
+    var grade = "";
+
+    if (element["label"].slice(-1) === "-") {
+      grade = element["label"].slice(0).toLowerCase() + "m";
+    } else {
+      grade = element["label"].toLowerCase();
+    }
+
+    if (element["enabled"]) {
+      if (!document.getElementById(`${grade}Check`).checked) {
+        document.getElementById(`${grade}Check`).click();
+      }
+    } else {
+      if (document.getElementById(`${grade}Check`).checked) {
+        document.getElementById(`${grade}Check`).click();
+      }
+    }
+
+    document.getElementById(`${grade}Spinner`).value = `${element["cutOff"]}`;
+    document
+      .getElementById(`${grade}Spinner`)
+      .dispatchEvent(new Event("input", { bubbles: false }));
+    document
+      .getElementById(`${grade}Spinner`)
+      .dispatchEvent(new Event("change", { bubbles: false }));
+  });
+});
