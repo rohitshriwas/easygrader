c4d2c4e5e01abb596a2763f4c41d5c84568efaf2
Saurabh Johri <saurabhxj@gmail.com>
2025-07-28 09:42:49 +0000

    EIPB-213 / Remove prefilled max marks and do not auto click plot histogram

--- scripts/cutoffs.js
+++ scripts/cutoffs.js
@@ -1,32 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   var cutOffs = JSON.parse(document.querySelector("[name='cut_offs']").value);
-  document.getElementById("plotHistButton").click();
 
-  cutOffs.forEach((element) => {
-    var grade = "";
+  var scores = document.getElementById("copyPasteData");
+  if (scores.value !== "") {
+    document.getElementById("plotHistButton").click();
 
-    if (element["label"].slice(-1) === "-") {
-      grade = element["label"].slice(0).toLowerCase() + "m";
-    } else {
-      grade = element["label"].toLowerCase();
-    }
+    cutOffs.forEach((element) => {
+      var grade = "";
 
-    if (element["enabled"]) {
-      if (!document.getElementById(`${grade}Check`).checked) {
-        document.getElementById(`${grade}Check`).click();
+      if (element["label"].slice(-1) === "-") {
+        grade = element["label"].slice(0).toLowerCase() + "m";
+      } else {
+        grade = element["label"].toLowerCase();
       }
-    } else {
-      if (document.getElementById(`${grade}Check`).checked) {
-        document.getElementById(`${grade}Check`).click();
-      }
-    }
 
-    document.getElementById(`${grade}Spinner`).value = `${element["cutOff"]}`;
-    document
-      .getElementById(`${grade}Spinner`)
-      .dispatchEvent(new Event("input", { bubbles: false }));
-    document
-      .getElementById(`${grade}Spinner`)
-      .dispatchEvent(new Event("change", { bubbles: false }));
-  });
+      if (element["enabled"]) {
+        if (!document.getElementById(`${grade}Check`).checked) {
+          document.getElementById(`${grade}Check`).click();
+        }
+      } else {
+        if (document.getElementById(`${grade}Check`).checked) {
+          document.getElementById(`${grade}Check`).click();
+        }
+      }
+
+      document.getElementById(`${grade}Spinner`).value = `${element["cutOff"]}`;
+      document
+        .getElementById(`${grade}Spinner`)
+        .dispatchEvent(new Event("input", { bubbles: true }));
+      document
+        .getElementById(`${grade}Spinner`)
+        .dispatchEvent(new Event("change", { bubbles: true }));
+    });
+  } else {
+    document.getElementById("courseTotalInput").value = "";
+  }
 });
